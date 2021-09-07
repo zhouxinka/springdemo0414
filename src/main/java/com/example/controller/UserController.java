dc5cf8ac2f9a887f37666332b052f2ff9f39a3b9
zhouxinka <61810813+zhouxinka@users.noreply.github.com>
2021-09-07 17:11:05 +0800

    修改注释
    修改注释

--- src/main/java/com/example/controller/UserController.java
+++ src/main/java/com/example/controller/UserController.java
@@ -67,7 +67,7 @@ public class UserController extends BaseController {
         System.out.println("UserController里面的doLogin方法的psd:"+psd);
         User user = userServiceImpl.getUserByName(username);
         if(user==null){
-            //增加flash信息提示用户名或者密码错误
+            //增加flash信息提示用户名或者密码错误，login页面也要增加sys:Lmessages标签
             addMessage(redirectAttributes,"登陆失败，用户名或密码错误！！！");
             return "redirect:"+Global.getAdminPath()+"/login";
         }
